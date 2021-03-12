e0328ed6b043eade56b3025f49963737e57d92da
DRY411S <dsryalls@gmail.com>
2021-03-12 19:28:44 +0000

    Up to number ranges

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 
 // Get the configuration
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 // Create a discord client
 const client = new Discord.Client();
@@ -11,15 +11,39 @@ const client = new Discord.Client();
 client.on('ready', readyDiscord);
 
 client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	// the rest of your code
 	console.log(message.content);
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
+	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+	// using the new `command` variable, this makes it easier to manage!
+	// you can switch your other commands to this format as well
+	else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} 	else if (args[0] === 'foo') {
+				return message.channel.send('bar');
+		}
+	
+		message.channel.send(`First argument: ${args[0]}`);
+	
+
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 	}
 });
 
 // Log the client in (need to hide ths token)
-client.login(config.token);
+client.login(token);
 
 // Logging function
 function readyDiscord() {
