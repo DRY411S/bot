120ec10c55147989b38be8a02762a6583645b744
DRY411S <dsryalls@gmail.com>
2021-03-13 15:05:16 +0000

    Dynamic command handler

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 // Require the discord module
 const Discord = require('discord.js');
+const fs = require('fs');
 
 // Get the configuration
 const { prefix, token } = require('./config.json');
 
-// Create a discord client
+// Create a discord client and get the commands
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+
+	// Get each command
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+
+		// set a new item in the Collection
+		// with the key as the command name and the value as the exported module
+		client.commands.set(command.name, command);
+	}
+}
 
 // Call this function every time this bot is started
-client.on('ready', readyDiscord);
+client.once('ready', readyDiscord);
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 	// the rest of your code
 	console.log(message.content);
-	if (message.content === `${prefix}ping`) {
-		message.channel.send('Pong.');
-	} else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop.');
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}
-	// using the new `command` variable, this makes it easier to manage!
-	// you can switch your other commands to this format as well
-	else if (command === 'args-info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		} 	else if (args[0] === 'foo') {
-				return message.channel.send('bar');
-		}
-	
-		message.channel.send(`First argument: ${args[0]}`);
-	
 
-		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	if (!client.commands.has(commandName)) 	return message.reply('I don\'t recognise that, sorry');
+
+	const command = client.commands.get(commandName);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
+
 });
 
 // Log the client in (need to hide ths token)
